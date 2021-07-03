01b6926f4f902d1fad5b766cf0fcd9551ae2471e
Nathnael <nttek28@gmail.com>
2021-07-03 22:37:28 +0400

    Jul3 late night

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -6,9 +6,12 @@
 /*   By: nghebreh <nathnael@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/29 19:04:24 by nghebreh          #+#    #+#             */
-/*   Updated: 2021/06/30 06:19:08 by nghebreh         ###   ########.fr       */
+/*   Updated: 2021/07/03 21:13:44 by nghebreh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <string.h>
+#include <stdio.h>
 
 int	ft_strcmp(char *s1, char *s2)
 {
@@ -18,4 +21,21 @@ int	ft_strcmp(char *s1, char *s2)
 		s2++;
 	}
 	return (*(unsigned char*)s1 - *(unsigned char*)s2);
+}
+
+int	main()
+{
+	char str1[] = "ABCDZEF";
+	char str2[] = "ABCDEF";
+	char str3[] = "ABCDZ";
+
+	printf("Comparing str1 & str2: %d\n", strcmp(str1, str2));
+	printf("Comparing str1 & str3: %d\n", strcmp(str1, str3));
+	printf("Comparing str3 & str1: %d\n", strcmp(str3, str1));
+
+	printf("Comparing str1 & str2: %d\n", ft_strcmp(str1, str2));
+	printf("Comparing str1 & str3: %d\n", ft_strcmp(str1, str3));
+	printf("Comparing str3 & str1: %d\n", ft_strcmp(str3, str1));
+
+
 }
--- c04/ex02/ft_putnbr.c
+++ c04/ex02/ft_putnbr.c
@@ -1,1 +1,17 @@
-void ft_putnbr(int nb);
+void ft_putnbr(int nb)
+{
+	if (nb == -2147483648)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		nb = 147483648;
+	}
+
+	else if (nb < 0)
+	{
+		ft_putchar('-');
+		nb *= 1;
+	}
+
+	
+}
