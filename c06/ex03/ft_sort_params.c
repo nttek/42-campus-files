1ee28a2a5e94b466da89455634db568cc0c95a67
Nathnael <nttek28@gmail.com>
2021-07-04 21:27:47 +0400

    July4 last updates

--- c06/ex03/ft_sort_params.c
+++ c06/ex03/ft_sort_params.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_rev_params.c                                    :+:      :+:    :+:   */
+/*   ft_sort_params.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nghebreh <nathnael@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/07/04 19:49:44 by nghebreh          #+#    #+#             */
-/*   Updated: 2021/07/04 21:12:44 by nghebreh         ###   ########.fr       */
+/*   Created: 2021/07/04 21:15:13 by nghebreh          #+#    #+#             */
+/*   Updated: 2021/07/04 21:26:55 by nghebreh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,15 +29,54 @@ void	ft_putstr(char *str)
 	}
 }
 
+int		ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 != '\0' && (*s1 == *s2))
+	{
+		s1++;
+		s2++;
+	}
+	return (*(unsigned char*)s1 - *(unsigned char*)s2);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int index;
+
+	index = 0;
+	while (src[index] != '\0')
+	{
+		dest[index] = src[index];
+		index++;
+	}
+	dest[index] = '\0';
+	return (dest);
+}
+
 int		main(int argc, char *argv[])
 {
-	while (argc > 1)
+	int i;
+	
+	char temp[30];
+  	for(int i=0; i<argc; i++)
 	{
-		ft_putstr(argv[argc - 1]);
+		for (int j=0; j<argc-1-i; j++)
+		{
+			if (ft_strcmp(argv[j], argv[j+1]) > 0)
+			{
+				ft_strcpy(temp, argv[j]);
+				ft_strcpy(argv[j], argv[j+1]);
+				ft_strcpy(argv[j+1], temp);
+			}
+		}
+	}
+	i = 1;
+	while (i < argc)
+	{
+		ft_putstr(argv[i]);
 		ft_putchar('\n');
-		argc--;
+		i++;
 	}
 
 	return (0);
 }
-
