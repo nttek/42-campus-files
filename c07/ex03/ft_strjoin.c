74370423ff5422244d07ef50bf6a8889c9da7bc2
Nathnael <nttek28@gmail.com>
2021-07-10 18:10:11 +0400

    ex07 first

--- c07/ex03/ft_strjoin.c
+++ c07/ex03/ft_strjoin.c
@@ -6,15 +6,26 @@
 /*   By: nghebreh <nathnael@gmail.com>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/09 17:32:56 by nghebreh          #+#    #+#             */
-/*   Updated: 2021/07/09 18:45:09 by nghebreh         ###   ########.fr       */
+/*   Updated: 2021/07/10 17:24:08 by nghebreh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strjoin(int size, char **strs, char *sep)
+int			ft_strlen(char *str)
+{
+	int count;
+
+	count = 0;
+	while (str[count] != '\0')
+		count++;
+	return (count);
+}
+
+char		*ft_strjoin(int size, char **strs, char *sep)
 {
 	int		i;
+	int		j;
 	int		k;
 	int		length;
 	char	*joined;
@@ -22,18 +33,20 @@ char	*ft_strjoin(int size, char **strs, char *sep)
 	i = 0;
 	length = 0;
 	while (i < size)
-	{
-		length += ft_strlen(str[i++]);
-	}
+		length += ft_strlen(strs[i++]);
 	joined = malloc((length + 1) * sizeof(char));
 	i = 0;
+	j = 0;
 	while (i < size)
 	{
-		while(strs[i])
-
+		k = 0;
+		while (strs[i][k])
+			joined[j++] = strs[i][k++];
+		k = 0;
+		while (sep[k] && i < size - 1)
+			joined[j++] = sep[k++];
+		i++;
 	}
-}
-
-int main()
-{
+	joined[j] = '\0';
+	return (joined);
 }
